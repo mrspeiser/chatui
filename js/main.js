c84355e12bb5ba2b2747ae0d6497bb12799fafc3
Matthew Speiser <mattrspeiser@gmail.com>
2017-08-08 14:19:12 -0700

    work in progress with some jquery cloning

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
-$('.user-text').on('click', function(){
-	$(this).parent().append('<div>helo world</div>');
+$(function () {
+
+
+	$('.user-container').click(function(event){
+		event.preventDefault();
+		var person = $(this);
+		var clone = person.addClass('user-chat-box').clone();
+		$('.chat-box').prepend(clone);
+	});
+
+	$('.chat-box').on('click', '.user-chat-box', function(){
+		$(this).toggleClass("show-thread");
+		$(this).append("<div class='message-thread'></div>");
+	});
 });
