1a00514f1deaa16626c30b31e41139e2315ecb9e
Matthew Speiser <mattrspeiser@gmail.com>
2017-08-10 21:07:34 -0700

    boss wanted a different design, bad communication

--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
-$(function () {
-
-
-	$('.chat-ui-container .user-room').click(function(event){
-		event.preventDefault();
-		var person = $(this);
-		var clone = person.addClass('user-chat-box').clone();
-		$('.chat-box').prepend(clone);
-	});
-
-	$('.chat-box').on('click', '.user-chat-box', function(){
-		$(this).children('.message-thread').toggleClass("displaynone");
-		// $(this).append("<div class='message-thread'></div>");
-	});
-
-
-	// $('.chat-box .user-room').on('click', function(){
-	// 	$(this).children('.message-thread').toggleClass('displaynone');
-	// });
+(function(){
 
 });
