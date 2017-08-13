d883c6cd20a04b95dd25f2025e13ee854cd69177
Matthew Speiser <mattrspeiser@gmail.com>
2017-08-12 20:56:22 -0700

    added jquery ui with resizeable on 2 elements

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
 (function(){
 
-});
+
+
+$( function() {
+    $( ".resizable1" ).resizable({
+      grid: 1,
+      maxWidth: 300,
+      handles: "e"
+    });
+  });
+
+$( function() {
+    $( ".resizable2" ).resizable({
+      grid: 1,
+      maxWidth: 300,
+      handles: "w"
+    });
+  });
+
+
+
+
+})();
