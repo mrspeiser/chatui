1967f911b3f9e0edd697b82c10bfa2187b1dbf79
Matthew Speiser <mattrspeiser@gmail.com>
2017-08-14 21:11:56 -0700

    working expands buttons left/right. Left menu has resizable, issue with right resizable

--- js/main.js
+++ js/main.js
@@ -6,41 +6,75 @@ $('#div1').resize(function(){
    $('#div2').width($("#parent").width()-newWidth); 
 });
 
-$("#div2").resizable({
-      handles: "e"
-    });
-$('#div2').resize(function(){
-	var newWidth = $("#div1").width() + $("#div2").width();
-   $('#div3').width($("#parent").width()-newWidth); 
-});
+
+// $("#div2").resizable({
+//       handles: "e"
+//     });
+// $('#div2').resize(function(){
+// 	var newWidth = $("#div1").width() + $("#div2").width();
+// 	var newWidthfix = newWidth+1;
+//    $('#div3').width($("#parent").width()-newWidthfix); 
+// });
 
 
-$(window).resize(function(){
-	var newWidth = $("#div1").width() + $("#div3").width();
-   $('#div2').width($("#parent").width()-newWidth); 
-   $('#div1').height($("#parent").height()); 
-});
+// $(window).resize(function(){
+// 	var newWidth = $("#div1").width() + $("#div3").width();
+//    $('#div2').width($("#parent").width()-newWidth); 
+//    $('#div1').height($("#parent").height()); 
+// });
 
 
-$("#div1togglewidth").on('click', function(){
+$("#button1").on('click', function(){
 	console.log('you clicked the button');
 	var minWidth = 40;
+	var toAddWidth = $("#div1").width() - 40;
 	var expandWidth = 285;
 	if($('#button1').hasClass('fa-arrow-circle-o-left')) {
-	 $('#button1').removeClass('fa-arrow-circle-o-left');
-	 $('#button1').addClass('fa-arrow-circle-o-right');
-	 $('#div1').width(minWidth);
+	  $('#button1').removeClass('fa-arrow-circle-o-left');
+	  $('#button1').addClass('fa-arrow-circle-o-right');
+	  $('#div1').width(minWidth);
+	  $('#div2').width($('#div2').width()+toAddWidth);
 	} else {
-	 $('#button1').addClass('fa-arrow-circle-o-left');
-	 $('#button1').removeClass('fa-arrow-circle-o-right');
-	 $('#div1').width(expandWidth);	
+	  $('#button1').addClass('fa-arrow-circle-o-left');
+	  $('#button1').removeClass('fa-arrow-circle-o-right');
+	  $('#div1').width(expandWidth);
+	  var div1Width = $('#div1').width();
+	  var div3Width = $('#div3').width();
+	  var expandWidth = div1Width + div3Width;
+	  var div2Width = $(window).width() - expandWidth;
+	  $('#div2').width(div2Width);
 	}
 
+});
+
+
+$("#button3").on('click', function(){
+	console.log('you clicked the button');
+	var minWidth = 40;
+	var toAddWidth = $("#div3").width() - 40;
+	var expandWidth = 285;
+	if($('#button3').hasClass('fa-arrow-circle-o-right')){
+	  $('#button3').removeClass('fa-arrow-circle-o-right');
+ 	  $('#button3').addClass('fa-arrow-circle-o-left');
+ 	  $('#div3').width(minWidth);
+ 	  $('#div2').width($('#div2').width()+toAddWidth);
+	} else {
+	  $('#button3').addClass('fa-arrow-circle-o-right');
+	  $('#button3').removeClass('fa-arrow-circle-o-left');
+	  $('#div3').width(expandWidth);
+	  var div1Width = $('#div1').width();
+	  var div3Width = $('#div3').width();
+	  var expandWidth = div1Width + div3Width;
+	  var div2Width = $(window).width() - expandWidth;
+	  $('#div2').width(div2Width);
+	}
+
+});
 	// if($('#button1').hasClass('fa-arrow-circle-o-left')){
 		
 	// } else {
 		
 	// }
-});
+
 
 })();
