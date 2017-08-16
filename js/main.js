80dbbe26a7979e8d094dd8aaad2337bf0a4c8f74
Matthew Speiser <mattrspeiser@gmail.com>
2017-08-15 22:31:52 -0700

    added more design

--- js/main.js
+++ js/main.js
@@ -56,17 +56,21 @@ $("#button3").on('click', function(){
 	if($('#button3').hasClass('fa-arrow-circle-o-right')){
 	  $('#button3').removeClass('fa-arrow-circle-o-right');
  	  $('#button3').addClass('fa-arrow-circle-o-left');
- 	  $('#div3').width(minWidth);
- 	  $('#div2').width($('#div2').width()+toAddWidth);
+ 	  // $('#div3').width(minWidth);
+ 	  // $('#div2').width($('#div2').width()+toAddWidth);
+ 	  $('#div3').css('width', '10%');
+ 	  $('#div2').css('width', '75%');
 	} else {
 	  $('#button3').addClass('fa-arrow-circle-o-right');
 	  $('#button3').removeClass('fa-arrow-circle-o-left');
-	  $('#div3').width(expandWidth);
-	  var div1Width = $('#div1').width();
-	  var div3Width = $('#div3').width();
-	  var expandWidth = div1Width + div3Width;
-	  var div2Width = $(window).width() - expandWidth;
-	  $('#div2').width(div2Width);
+	  $('#div3').css('width', '20%');
+ 	  $('#div2').css('width', '65%');
+	  // $('#div3').width(expandWidth);
+	  // var div1Width = $('#div1').width();
+	  // var div3Width = $('#div3').width();
+	  // var expandWidth = div1Width + div3Width;
+	  // var div2Width = $(window).width() - expandWidth;
+	  // $('#div2').width(div2Width);
 	}
 
 });
